15a79468dd24b65a5b03a1680746194af62472f0
CruiseDevice <meakashchavan@gmail.com>
2020-04-04 23:53:31 +0530

    Add Like and delete feature
     - User can delete his own uploaded pins.
     - User can like his own uploaded as well as other users pins.

--- pinterest/urls.py
+++ pinterest/urls.py
@@ -6,6 +6,7 @@ app_name="pinterest"
 
 urlpatterns = [
     url(r'^create/$',views.image_create,name='create'),
+    url(r'^like_image/$', views.like_image, name='like_image'),
     url(r'^delete/(?P<id>\d+)/$', views.delete_image, name='delete_image'),
     url(r'^my_post/$',views.my_post, name='my_post'),
     url(r'^',views.index,name='index'),
